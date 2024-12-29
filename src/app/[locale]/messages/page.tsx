92db986a88d98ee54f783e8462617d9da39e8c96
patson120 <patrick1kenne@gmail.com>
2024-12-29 14:27:07 +0100

    Mise à jour

--- src/app/[locale]/messages/page.tsx
+++ src/app/[locale]/messages/page.tsx
@@ -24,7 +24,7 @@ export default function Page() {
                 />
                 <div className="absolute inset-0 bg-black/40">
                     <div className='container max-margin py-0 h-full flex flex-col justify-center items-center'>
-                        <div className='w-full h-full flex flex-col justify-between'>
+                        <div className='w-full h-full flex flex-col justify-between translate-y-10 md:translate-y-0 '>
                             <div className='' />
                             <div className='flex flex-col md:flex-row justify-start items-center gap-8 xl:gap-10'>
                                 <div className='h-64 w-64 shrink-0 relative overflow-hidden border-2 border-gray-400'>
@@ -39,15 +39,17 @@ export default function Page() {
                                         }}
                                     />
                                 </div>
-                                <div className='space-y-2 w-1/2'>
-                                    <h5 className='heading-2 text-white font-extrabold'>Communiqués du bureau de Mgr. Desrochers</h5>
-                                    <p className='body-2 text-gray-200 line-clamp-2'>Veuillez cliquer sur les liens suivants pour accéder aux communiqués de notre archevêque, Mgr Guy Desrochers</p>
+                                <div className='space-y-2 lg:w-1/2'>
+                                    <h5 className='heading-2 md:text-white font-extrabold'>Communiqués du bureau de Mgr. Desrochers</h5>
+                                    <p className='body-2 md:text-gray-200 line-clamp-2'>Veuillez cliquer sur les liens suivants pour accéder aux communiqués de notre archevêque, Mgr Guy Desrochers</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
+
+            <div className='my-44 md:my-0' />
 
             <section className='container max-margin py-0 pb-10 md:pb-20'>
 
@@ -82,35 +84,33 @@ export default function Page() {
 
 const Filter = () => {
     return (
-        <div className='flex justify-start items-center gap-2'>
-            <div className='flex items-center gap-2 lg:gap-4'>
-                <div className='flex items-center gap-[6px]'>
-                    <SlidersHorizontalIcon className="h-4 w-4 text-gray-600" />
-                    <label className='font-bold whitespace-nowrap' htmlFor="filter">Filtrer les résultats</label>
-                </div>
-                <select id="mois" name="mois" className="border border-gray-100 outline-primary outline-offset-1 px-3 py-2 rounded-lg w-full">
-                    <option value="" disabled>Par date</option>
-                    <option value="">Par date</option>
-                    <option value="janvier">Janvier</option>
-                    <option value="fevrier">Février</option>
-                    <option value="mars">Mars</option>
-                    <option value="avril">Avril</option>
-                    <option value="mai">Mai</option>
-                    <option value="juin">Juin</option>
-                    <option value="juillet">Juillet</option>
-                    <option value="aout">Août</option>
-                    <option value="septembre">Septembre</option>
-                    <option value="actobre">Octobre</option>
-                    <option value="novembre">Novembre</option>
-                    <option value="decembre">Décembre</option>
-                </select>
-                <select id="categorie" name="categorie" className="border border-gray-100 outline-primary outline-offset-1 px-3 py-2 rounded-lg w-full">
-                    <option value="" disabled>Par type</option>
-                    <option value="">Par type</option>
-                    <option value="actualite">Actualité</option>
-                    <option value="evenement">Evènement</option>
-                </select>
+        <div className='flex flex-col md:flex-row md:items-center gap-3 lg:gap-4 w-full md:w-auto'>
+            <div className='flex items-center gap-[6px]'>
+                <SlidersHorizontalIcon className="h-5 w-5 text-gray-600" />
+                <label className='font-bold whitespace-nowrap' htmlFor="filter">Filtrer les résultats</label>
             </div>
+            <select id="mois" name="mois" className="border border-gray-100 outline-primary outline-offset-1 px-3 py-2 rounded-lg w-full md:w-auto">
+                <option value="" disabled>Par date</option>
+                <option value="">Par date</option>
+                <option value="janvier">Janvier</option>
+                <option value="fevrier">Février</option>
+                <option value="mars">Mars</option>
+                <option value="avril">Avril</option>
+                <option value="mai">Mai</option>
+                <option value="juin">Juin</option>
+                <option value="juillet">Juillet</option>
+                <option value="aout">Août</option>
+                <option value="septembre">Septembre</option>
+                <option value="actobre">Octobre</option>
+                <option value="novembre">Novembre</option>
+                <option value="decembre">Décembre</option>
+            </select>
+            <select id="categorie" name="categorie" className="border border-gray-100 outline-primary outline-offset-1 px-3 py-2 rounded-lg w-full md:w-auto">
+                <option value="" disabled>Par type</option>
+                <option value="">Par type</option>
+                <option value="actualite">Actualité</option>
+                <option value="evenement">Evènement</option>
+            </select>
         </div>
     )
 }
