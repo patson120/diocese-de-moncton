1da8486d3c9d58a9c9739f348f83fe8a58bc2a74
patson120 <patrick1kenne@gmail.com>
2024-12-29 07:10:22 +0100

    Mise à jour

--- src/components/ui/shared/footer.tsx
+++ src/components/ui/shared/footer.tsx
@@ -74,7 +74,7 @@ export default function Footer() {
       </div>
       <div className='container max-margin py-0 mt-10 flex flex-col md:flex-row md:justify-between gap-5 md:gap-3 '>
         <p className='order-1 md:order-2 pt-1 body-3 text-center md:text-left'>© {date.getFullYear()} Diocèse Moncton - Tous droits reservés</p>
-        <div className='flex justify-center items-center gap-4 lg:gap-6'>
+        <div className='order-2 md:order-1 flex justify-center items-center gap-4 lg:gap-6'>
           <FacebookIcon className="h-4 w-4 md:h-6 md:w-6 lg:h-7 lg:w-7" />
           <TwitterIcon className="h-4 w-4 md:h-6 md:w-6 lg:h-7 lg:w-7" />
           <InstagramIcon className="h-4 w-4 md:h-6 md:w-6 lg:h-7 lg:w-7" />
