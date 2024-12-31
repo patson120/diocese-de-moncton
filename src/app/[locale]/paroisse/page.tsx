59b40763cc9260141f8de57bd28ca1481c79f46e
patson120 <patrick1kenne@gmail.com>
2024-12-31 05:48:01 +0100

    Mise à jour

--- src/app/[locale]/paroisse/page.tsx
+++ src/app/[locale]/paroisse/page.tsx
@@ -32,47 +32,46 @@ const parishes = [
 export default function Page() {
     const [selectedParish, setSelectedParish] = useState<any>(null);
     return (
-        <>
-            <div className='container px-0 py-0 my-10'>
-                <div className='flex justify-center items-center gap-2 my-7'>
-                    <div className='w-1/2 flex gap-2'>
-                        <div className='relative flex-1'>
-                            <input type="text" placeholder="Entrez le code postale ou la ville..."
-                                className="w-full border border-gray-100 bg-gray-50 rounded-lg pr-3 pl-10 py-2
+        <section className='container max-margin vertical-margin pt-5 mt-0'>
+            <div className='flex justify-center items-center gap-2 my-7'>
+                <div className='w-full lg:w-1/2 flex gap-2'>
+                    <div className='relative flex-1'>
+                        <input type="text" placeholder="Entrez le code postale ou la ville..."
+                            className="w-full border border-gray-100 bg-gray-50 rounded-lg pr-3 pl-10 py-2
                                 text-gray-900 ring-1 ring-inset ring-gray-50 placeholder:text-gray-400
                                 placeholder:text-xs sm:text-sm sm:leading-6 outline-none"/>
 
-                            <Search className="mr-2 h-4 w-4 text-gray-300 absolute top-3 left-3" />
-                        </div>
-                        <Button variant="secondary" className='bg-[#1D0104] text-[12px] text-white hover:bg-[#230105] ' onClick={() => { }}>
-                            <MapPin className="mr-2 h-4 w-4 text-white" />
-                            Prendre ma position
-                        </Button>
+                        <Search className="mr-2 h-4 w-4 text-gray-300 absolute top-3 left-3" />
                     </div>
+                    <Button variant="secondary" className='bg-[#1D0104] text-[12px] text-white hover:bg-[#230105] ' onClick={() => { }}>
+                        <MapPin className="md:mr-2 h-4 w-4 text-white" />
+                        <span className='hidden md:flex'>Prendre ma position</span>
+                    </Button>
                 </div>
-                <div className='border-t border-gray-100'>
-                    <div className='h-[60vh] grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-6 lg:gap-10 xl:gap-12 '>
-                        <div className='col-span-1 flex flex-col gap-3 pb-3 overflow-y-scroll'>
-                            <div className='sticky top-0 bg-white z-20 py-3'>
-                                <Filter />
-                            </div>
-                            {
-                                [...paroisses, ...paroisses].map((item, index) => (
-                                    <ParoisseItem key={index} data={item} />
-                                ))
-                            }
+            </div>
+            <div className='my-10'/>
+            <div className='border-t border-gray-100'>
+                <div className='h-[100vh] md:h-[50vh] lg:h-[60vh] grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-6 lg:gap-10 xl:gap-12 '>
+                    <div className='col-span-1 flex flex-col gap-3 pb-3 overflow-y-scroll'>
+                        <div className='sticky top-0 bg-white z-20 py-3'>
+                            <Filter />
                         </div>
-                        <div className='col-span-2 bg-gray-100 overflow-hidden'>
-                            <Map
-                                parishes={parishes}
-                                selectedParish={selectedParish}
-                                onParishSelect={setSelectedParish}
-                            />
-                        </div>
+                        {
+                            [...paroisses, ...paroisses].map((item, index) => (
+                                <ParoisseItem key={index} data={item} />
+                            ))
+                        }
+                    </div>
+                    <div className='h-[50vh] md:h-auto col-span-2 bg-gray-100 overflow-hidden'>
+                        <Map
+                            parishes={parishes}
+                            selectedParish={selectedParish}
+                            onParishSelect={setSelectedParish}
+                        />
                     </div>
                 </div>
             </div>
-        </>
+        </section>
     )
 }
 
