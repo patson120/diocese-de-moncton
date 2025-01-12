c1c123f0651bb6214cc5b6cb9a3ac743fcfa00fd
patson120 <patrick1kenne@gmail.com>
2025-01-12 21:33:32 +0100

    Mise à jour

--- src/app/[locale]/evenement/[evenementId]/page.tsx
+++ src/app/[locale]/evenement/[evenementId]/page.tsx
@@ -32,48 +32,52 @@ export default function Page() {
                 </div>
             </div>
             <section className="container px-0 py-0">
-                <div className='grid grid-cols-1 lg:grid-cols-5 gap-6 lg:gap-12 py-4 lg:py-8'>
+                <div className='grid grid-cols-1 lg:grid-cols-5 md:gap-6 lg:gap-12 md:py-4 lg:py-8'>
                     <div className='col-span-full lg:col-span-2'>
-                        <div className='h-80 relative rounded-lg overflow-hidden bg-gray-100'>
+                        <div className='h-80 xl:h-96 relative md:rounded-[18px] overflow-hidden bg-gray-100'>
                             <Image
                                 alt="Image de détail de l'évènement"
-                                src="/assets/img/soupe-homard.png"
+                                src="/assets/img/event-details.png"
                                 fill
                                 style={{ objectFit: 'cover' }}
                             />
                         </div>
                     </div>
-                    <div className='col-span-full lg:col-span-3'>
+                    <div className='container max-margin md:px-0 md:mx-0 col-span-full lg:col-span-3'>
                         <div>
-                            <small className='text-gray-600'>Titre évènement</small>
-                            <h1 className='text-xl font-extrabold mb-4'>Soupé au homard</h1>
-                            <p className='text-sm text-gray-600'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe maiores sit ex harum. Reprehenderit labore aspernatur iste voluptatem. Qui reprehenderit doloribus est, odio quisquam impedit mollitia aut excepturi officia. Optio id ex excepturi eum, accusamus voluptatum eveniet iste, sed beatae tempora unde deleniti molestiae suscipit numquam autem facere rerum et perspiciatis animi illo quo facilis qui asperiores magnam! Ratione impedit laborum eum nisi accusamus autem dolor, sit incidunt quia sed.</p>
+                            <small className='text-gray'>Titre évènement</small>
+                            <h1 className='heading-4 font-extrabold mb-4'>Grandir ensemble 2025</h1>
+                            <p className='body-2 text-gray line-clamp-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe maiores sit ex harum. Reprehenderit labore aspernatur iste voluptatem. Qui reprehenderit doloribus est, odio quisquam impedit mollitia aut excepturi officia. Optio id ex excepturi eum, accusamus voluptatum eveniet iste, sed beatae tempora unde deleniti molestiae suscipit numquam autem facere rerum et perspiciatis animi illo quo facilis qui asperiores magnam! Ratione impedit laborum eum nisi accusamus autem dolor, sit incidunt quia sed.</p>
                         </div>
-                        <div className='flex flex-wrap space-x-3 items-center font-light text-xs my-4'>
-                            <label htmlFor="type_evenement">Type évènement</label>
-                            <p className='px-4 py-1 text-gray-500 rounded-full border border-gray-100'>Formation</p>
-                            <p className='px-4 py-1 text-gray-500 rounded-full border border-gray-100'>Célébration</p>
-                            <p className='px-4 py-1 text-gray-500 rounded-full border border-gray-100'>Communautaire</p>
+                        <div className='flex flex-wrap gap-3 items-center font-light text-xs my-4'>
+                            <label htmlFor="type_evenement" className='body-2 font-bold w-full md:w-auto'>Type évènement</label>
+                            <p className='px-4 py-3 text-gray text-sm rounded-xl border border-gray-100'>Formation</p>
+                            <p className='px-4 py-3 text-gray text-sm rounded-xl border border-gray-100'>Célébration</p>
+                            <p className='px-4 py-3 text-gray text-sm rounded-xl border border-gray-100'>Communautaire</p>
                         </div>
-                        <hr className='my-2' />
-                        <div className='flex space-x-8 mt-4'>
+                        <div className='my-4 border-b border-b-[#E5E5E5]' />
+                        <div className='flex flex-col md:flex-row gap-8 mt-4'>
                             <div className='space-y-1'>
-                                <div className='flex items-center space-x-2'>
-                                    <Calendar className="h-4 w-4 text-gray-400" />
-                                    <label htmlFor="date" className='text-xs text-gray-500'>Date</label>
+                                <div className='flex items-center space-x-1'>
+                                    <Calendar className="h-5 w-5 text-gray-400" />
+                                    <label htmlFor="date" className='text-gray'>Date</label>
                                 </div>
-                                <h1 className='text-[11px] font-semicold'>Dimanche 29 Septembre</h1>
+                                <h1 className='text-base font-semibold'>Dimanche 29 Septembre</h1>
                             </div>
                             <div className='space-y-1'>
-                                <div className='flex space-x-2'>
-                                    <Timer className="h-4 w-4 text-gray-400" />
-                                    <label htmlFor="heure" className='text-xs text-gray-500'>Heure</label>
+                                <div className='flex space-x-1'>
+                                    <Timer className="h-5 w-5 text-gray-400" />
+                                    <label htmlFor="heure" className=' text-gray-500'>Heure</label>
                                 </div>
-                                <h1 className='text-[11px] font-semibold'>16h00 à 18h00</h1>
+                                <h1 className='text-base font-semibold'>16h00 à 18h00</h1>
                             </div>
+                        </div>
+                        <div>
+                            <h1 className="heading-4 font-extrabold text-black mt-10 mb-2">Lieu</h1>
+                            <p className='body-2 text-gray'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                         </div>
 
-                        <h1 className="text-lg font-extrabold text-black mt-10 mb-2">Lieu: <span className='text-sm font-normal text-gray-600'>Pointe Sapin</span></h1>
+                        <h1 className="heading-4 font-extrabold text-black mt-10 mb-2">Sur la carte</h1>
                         {/* Map */}
                         <div className="h-80 w-full bg-gray-100 rounded-xl overflow-hidden">
                             <Map
