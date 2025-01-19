10c89095f7d716832f657af4b21d6d24ec4d65f4
patson120 <patrick1kenne@gmail.com>
2025-01-19 21:30:42 +0100

    Mise à jour des pages

--- src/components/ui/shared/newsletter.tsx
+++ src/components/ui/shared/newsletter.tsx
@@ -9,7 +9,7 @@ export default function Newsletter() {
       <div className='container max-margin py-0 flex flex-col md:flex-row justify-between gap-3'>
         <div className='text-black'>
           <h3 className='body-1 font-bold'>Newsletter</h3>
-          <p className='text-[#575757] body-2'>Abonnez-vous à notre newsletter pour obtenir les <br />nouveautés sur l&lsquo;église.</p>
+          <p className='text-[#575757] body-2'>Abonnez-vous à notre newsletter pour obtenir les <br className='hidden lg:block' />nouveautés sur l&lsquo;église.</p>
         </div>
         <div className='flex flex-col md:flex-row items-end md:items-center space-y-3 md:space-y-0 md:space-x-2'>
           <input type="email" placeholder="Adresse email" className="border-2 border-gray-200 p-2 rounded-[12px] w-full" />
