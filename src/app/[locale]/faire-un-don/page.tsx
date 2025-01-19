626cd6d4959b3920bbb7df75fbb18bd042c3bea2
patson120 <patrick1kenne@gmail.com>
2025-01-19 21:22:03 +0100

    Mise à jour

--- src/app/[locale]/faire-un-don/page.tsx
+++ src/app/[locale]/faire-un-don/page.tsx
@@ -51,35 +51,85 @@ export default function Page() {
                                         <div className='flex flex-row space-x-1'>
                                             {
                                                 [1, 2, 3].map((item) => (
-                                                    <div key={item} className={`${step >= item  ? 'opacity-100': 'opacity-20'} h-[5px] w-7 rounded-full bg-[#1D0104] `}></div>
+                                                    <div key={item} className={`${step >= item ? 'opacity-100' : 'opacity-20'} h-[5px] w-7 rounded-full bg-[#1D0104] `}></div>
                                                 ))
                                             }
                                         </div>
                                     </div>
                                 </div>
-                                <div className='px-6 space-y-5'>
-                                    <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
-                                        <div className=''>
-                                            <label className='inline-block font-semibold mb-2' htmlFor="nom">Nom</label>
-                                            <input type="text" placeholder="Entrez votre nom" className="border-2 border-gray-200 p-2 rounded-xl w-full" />
+                                <div className='px-6'>
+                                    {
+                                        (step === 1) &&
+                                        <div className='space-y-5'>
+                                            <div className='flex flex-row space-x-2'>
+                                                {
+                                                    [10, 15, 50, 100].map(item => (
+                                                        <Button variant='outline' onClick={() => { }} className="w-auto px-3 py-2 body-3">
+                                                            {item}$
+                                                        </Button>
+                                                    ))
+                                                }
+                                                <input type="number" placeholder="Ou entrez un montant..." className="border-2 border-gray-200 flex-1 p-2 rounded-xl w-full" />
+                                            </div>
+                                            <p className=''>Nous recevons finalement <span className='font-bold'>xx$</span> après déduction fiscale</p>
                                         </div>
-                                        <div className=''>
-                                            <label className='inline-block font-semibold mb-2' htmlFor="prenom">Prénom</label>
-                                            <input type="text" placeholder="Entrez votre prénom" className="border-2 border-gray-200 p-2 rounded-xl w-full" />
+                                    }
+
+                                    {
+                                        (step === 2) &&
+                                        <div className='space-y-5'>
+                                            <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+                                                <div className=''>
+                                                    <label className='inline-block font-semibold mb-2' htmlFor="nom">Nom</label>
+                                                    <input type="text" placeholder="Entrez votre nom" className="border-2 border-gray-200 p-2 rounded-xl w-full" />
+                                                </div>
+                                                <div className=''>
+                                                    <label className='inline-block font-semibold mb-2' htmlFor="prenom">Prénom</label>
+                                                    <input type="text" placeholder="Entrez votre prénom" className="border-2 border-gray-200 p-2 rounded-xl w-full" />
+                                                </div>
+                                            </div>
+                                            <div className=''>
+                                                <label className='inline-block font-semibold mb-2' htmlFor="email">Email</label>
+                                                <input type="email" placeholder="Adresse email" className="border-2 border-gray-200 p-2 rounded-xl w-full" />
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className=''>
-                                        <label className='inline-block font-semibold mb-2' htmlFor="email">Email</label>
-                                        <input type="email" placeholder="Adresse email" className="border-2 border-gray-200 p-2 rounded-xl w-full" />
-                                    </div>
-                                    <div className='flex flex-col-reverse gap-4 md:flex-row md:justify-between items-center'>
-                                        <Button onClick={() => { }} className="w-full md:w-auto px-3 md:px-6 py-2 body-3">
-                                            {/* Je m&lsquo;abonne */}
-                                            Etape suivante
-                                        </Button>
-                                        <Button variant='ghost' onClick={() => { }} className="w-full md:w-auto px-3 md:px-6 py-2 body-3">
-                                            Retour
-                                        </Button>
+                                    }
+                                    {
+                                        (step === 3) &&
+                                        <div className='space-y-5'>
+                                            <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+                                                <div className=''>
+                                                    <label className='inline-block font-semibold mb-2' htmlFor="nomeroCarte">Numéro de carte</label>
+                                                    <input type="number" placeholder="0000 - 0000 - 0000 - 0000" className="border-2 border-gray-200 p-2 rounded-xl w-full" />
+                                                </div>
+                                                <div className=''>
+                                                    <label className='inline-block font-semibold mb-2' htmlFor="validity">Validity</label>
+                                                    <input type="date" placeholder="MM/YY" className="border-2 border-gray-200 p-2 rounded-xl w-full" />
+                                                </div>
+                                            </div>
+                                            <div className=''>
+                                                <label className='inline-block font-semibold mb-2' htmlFor="cvc">CVC</label>
+                                                <input type="text" placeholder="CVC Code" className="border-2 border-gray-200 p-2 rounded-xl w-full" />
+                                            </div>
+                                        </div>
+                                    }
+                                    <div className='flex flex-col-reverse gap-4 md:flex-row md:justify-between items-center pt-10'>
+
+                                        {
+                                            (step === 3) ?
+                                                <Button onClick={() => { }} className="w-full md:w-auto px-3 md:px-6 py-2 body-3">
+                                                    Faire un don
+                                                </Button> :
+                                                <Button onClick={() => { setStep(prev => prev + 1) }} className="w-full md:w-auto px-3 md:px-6 py-2 body-3">
+                                                    Etape suivante
+                                                </Button>
+                                        }
+                                        {
+                                            (step > 1) &&
+                                            <Button variant='ghost' onClick={() => { setStep(prev => prev - 1) }} className="w-full md:w-auto px-3 md:px-6 py-2 body-3">
+                                                Retour
+                                            </Button>
+                                        }
                                     </div>
                                 </div>
 
