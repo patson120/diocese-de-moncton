cd88df5702905a5283ac2664325575ebb7e7891e
patson120 <patrick1kenne@gmail.com>
2025-02-24 11:06:23 +0100

    Mise à jour des pages

--- src/_lib/data.ts
+++ src/_lib/data.ts
@@ -7,9 +7,9 @@ export async function fetchActualites() {
         let url = `${BASE_URL}/actualites`
         let body = {}
         const headers = {}
-        await new Promise((resolve, reject) => {
-            setTimeout(() => resolve(true), 10000); // 10 seconds timeout
-        })
+        // await new Promise((resolve, reject) => {
+        //     setTimeout(() => resolve(true), 10000); // 10 seconds timeout
+        // })
         return await fetchAPI(url, 'GET', headers, body);
 
     } catch (error) {
@@ -27,9 +27,7 @@ export async function fetchParoisses() {
         // await new Promise((resolve, reject) => {
         //     setTimeout(() => resolve(true), 1000); // 5 seconds timeout
         // },)
-        setTimeout( async () => {
-            return await fetchAPI(url, 'GET', headers, body);
-        }, 5000);
+        return await fetchAPI(url, 'GET', headers, body);
 
     } catch (error) {
         console.error('Database Error:', error);
@@ -46,9 +44,7 @@ export async function fetchEvents() {
         // await new Promise((resolve, reject) => {
         //     setTimeout(() => resolve(true), 3000); // 3 seconds timeout
         // },)
-        setTimeout( async () => {
-            return await fetchAPI(url, 'GET', headers, body);
-        }, 5000);
+        return await fetchAPI(url, 'GET', headers, body);
 
     } catch (error) {
         console.error('Database Error:', error);
@@ -65,9 +61,7 @@ export async function fetchArcheveques() {
         // await new Promise((resolve, reject) => {
         //     setTimeout(() => resolve(true), 1000); // 1 seconds timeout
         // },)
-        setTimeout( async () => {
-            return await fetchAPI(url, 'GET', headers, body);
-        }, 5000);
+        return await fetchAPI(url, 'GET', headers, body);
 
     } catch (error) {
         console.error('Database Error:', error);
