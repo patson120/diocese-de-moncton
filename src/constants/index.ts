812d489407ed7305a73d447d76759a6c62900bf1
patson120 <patrick1kenne@gmail.com>
2025-03-09 19:15:51 +0100

    Mise à jour des API

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-import { ActualiteType, EventType, MenuType, MessageType, ParoisseType } from "@/types"
+import { EventType, MenuType, MessageType, ParoisseType } from "@/types"
 
 export const NEXT_PUBLIC_GOOGLE_MAPS_API_KEY = 'AIzaSyApdnBLqJeVW4c5tlZ32v8BzVBVWyJnYlg'
  export const BASE_URL = "http://diocese.wds-project.com/api"
@@ -278,99 +278,6 @@ export const ressources: MenuType[] = [
     // }
 ]
 
-// List of news
-export const news: ActualiteType[] = [
-    {
-        category: "Diocèse",
-        title: "Quel est le problème avec l’aide médicale à mourir (AMM)",
-        date: "Lundi 25 Octobre 2022",
-        image: "/assets/img/new-1.png",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt reiciendis vero repudiandae eveniet, animi nisi odio totam ratione iste debitis."
-    },
-    {
-        category: "Diocèse",
-        title: "Quête spéciale le 2-3 novembre 2024",
-        date: "Mardi 26 Octobre 2022",
-        image: "/assets/img/new-2.png",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt reiciendis vero repudiandae eveniet, animi nisi odio totam ratione iste debitis."
-    },
-    {
-        category: "Catéchèse",
-        title: "Mise à jour des tarifs diocésains pour les célébr...",
-        date: "Mardi 26 Octobre 2022",
-        image: "/assets/img/new-3.png",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt reiciendis vero repudiandae eveniet, animi nisi odio totam ratione iste debitis."
-    },
-    {
-        category: "Diocèse",
-        title: "Nominations ecclésiales pour le diocèse de Moncton",
-        date: "Mardi 26 Octobre 2022",
-        image: "/assets/img/new-4.png",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt reiciendis vero repudiandae eveniet, animi nisi odio totam ratione iste debitis."
-    },
-];
-
-export const paroisses: ParoisseType[] = [
-    {
-        title: "Immaculée-Conception",
-        adresse: "4049, Route 480 Acadieville NB E4Y 1Z3",
-        tel: "(506) 775-2421",
-        image: "/assets/img/paroisse-1.png"
-    },
-    {
-        title: "Saint-Anne",
-        adresse: "4049, Route 480 Acadieville NB E4Y 1Z3",
-        tel: "(506) 775-2421",
-        image: "/assets/img/paroisse-2.png"
-    },
-    {
-        title: "Saint-Baptiste",
-        adresse: "4049, Route 480 Acadieville NB E4Y 1Z3",
-        tel: "(506) 775-2421",
-        image: "/assets/img/paroisse-3.png"
-    },
-    {
-        title: "Notre Dame de Fatima",
-        adresse: "4049, Route 480 Acadieville NB E4Y 1Z3",
-        tel: "(506) 775-2421",
-        image: "/assets/img/paroisse-1.png"
-    },
-]
-
-export const events: EventType[] = [
-    {
-        jour: "08",
-        heure: "15h00",
-        mois: "Sept",
-        lieu: "Au diocèse de Moncton",
-        title: "Soupé au homard à pointe Sapin",
-        type: "Communautaire, Formation, Célébration"
-    },
-    {
-        jour: "14",
-        heure: "08h00",
-        mois: "Sept",
-        lieu: "Au diocèse de Moncton",
-        title: "Trending the soil, tending the sowe...",
-        type: "Communautaire, Formation, Célébration"
-    },
-    {
-        jour: "14",
-        heure: "10h00",
-        mois: "Sept",
-        lieu: "Au diocèse de Moncton",
-        title: "40e anniversaire du pape Jean Pa...",
-        type: "Communautaire, Formation, Célébration"
-    },
-    // {
-    //     jour: "23",
-    //     heure: "14h00",
-    //     mois: "Sept",
-    //     lieu: "Au diocèse de Moncton",
-    //     title: "Monday the movies",
-    //     type: "Communautaire, Formation, Célébration"
-    // },
-]
 
 export const messages: MessageType[] = [
     {
