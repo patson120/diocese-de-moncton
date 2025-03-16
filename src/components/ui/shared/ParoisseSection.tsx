beca3c4aa09b4a75a45692aacf02e6e203994ce9
patson120 <patrick1kenne@gmail.com>
2025-03-16 17:23:20 +0100

    Mise à jour des elemets

--- src/components/ui/shared/ParoisseSection.tsx
+++ src/components/ui/shared/ParoisseSection.tsx
@@ -9,7 +9,7 @@ import { ParoisseItemSkeleton } from './skeletons'
 export default async function ParoisseSection({ query}: { query: string}) {
     const response = await fetchParoisses(`?paginate=4&nom=${query}`)
     const paroisses: Paroisse[] = response.data
-
+    
     return (
         <div className="vertical-margin" >
             <h1 className="heading-3 mb-3">Trouver une paroisse</h1>
@@ -22,7 +22,7 @@ export default async function ParoisseSection({ query}: { query: string}) {
                     <section className="col-span-3 md:col-span-1 flex flex-col gap-4">
                         { [1, 2, 3, 4, 5].map(i => <ParoisseItemSkeleton key={i} />)}
                     </section>}>
-                    <RecentParoisses paroisses={paroisses} total={paroisses.length} />
+                    <RecentParoisses paroisses={paroisses} total={response.total} />
                 </Suspense>
             </section>
         </ div>
--- src/components/ui/shared/SearchParoisses.tsx
+++ src/components/ui/shared/SearchParoisses.tsx
@@ -1,11 +1,12 @@
 "use client"
 
 import { MapPin, Search } from 'lucide-react'
-import React, { ChangeEvent } from 'react'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 import { Button } from './button'
 import { useDebouncedCallback } from 'use-debounce'
 import { useSearchParams } from 'next/navigation'
 import { usePathname, useRouter } from '@/i18n/routing'
+import { getLocationOrigin } from 'next/dist/shared/lib/utils'
 
 export default function SearchParoisses() {
 
@@ -21,6 +22,45 @@ export default function SearchParoisses() {
         replace(`${pathname}?${params.toString()}#evenements`)
     }, 800)
 
+    const [isActive, setIsActive] = useState(false)
+    const [userLatitude, setUserLatitude] = useState<number>()
+    const [userLongitude, setUserLongitude] = useState<number>()
+    const [positionError, setPositionError] = useState<any>()
+    const [isLoading, setIsLoading] = useState(false)
+
+
+    const fetchGeoPosition = () => {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                if (isActive) {
+                    setUserLatitude(position.coords.latitude);
+                    setUserLongitude(position.coords.longitude);
+                    setPositionError(null);
+                }
+                setIsLoading(false)
+            },
+            error =>{
+                 isActive && setPositionError(error.message)
+                console.log({error});
+                
+                },
+            { enableHighAccuracy: true, timeout: 5*60*60, maximumAge: 10000 }
+        );
+    }
+
+
+    // Récupération de la position géographique de l'utilisateur
+    useEffect(() => {
+        if (isActive) {
+            setIsLoading(true)
+            fetchGeoPosition()
+        }
+        return () => setIsActive(false)
+    }, [isActive])
+
+    const getLocation = () => {
+        setIsActive(true)
+    }
     return (
         <div className='w-full absolute top-4 left-0 right-0'>
             <div className="px-5 w-[95%] flex gap-2">
@@ -34,7 +74,7 @@ export default function SearchParoisses() {
                                     placeholder:text-xs sm:text-sm sm:leading-6 outline-none"/>
                     <Search className="mr-2 h-4 w-4 text-gray-300 absolute top-3 left-3" />
                 </div>
-                <Button variant="secondary" className='bg-[#1D0104] text-[12px] text-white hover:bg-[#230105]'>
+                <Button onClick={getLocation} variant="secondary" className='bg-[#1D0104] text-[12px] text-white hover:bg-[#230105]'>
                     <MapPin className="md:mr-2 h-4 w-4 text-white" />
                     <span className='hidden md:flex !text-white'>Prendre ma position</span>
                 </Button>
