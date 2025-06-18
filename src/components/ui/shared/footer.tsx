692241c2a1e06cb2e8bc369f588c50550a8d3e9b
patson120 <patrickkennenl@gmail.com>
2025-06-18 09:51:44 +0100

    Ajout de la traduction sur le footer

--- src/components/ui/shared/footer.tsx
+++ src/components/ui/shared/footer.tsx
@@ -1,3 +1,4 @@
+import Text from '@/components/Text'
 import { Link } from '@/i18n/routing'
 import { FacebookIcon, InstagramIcon, TwitterIcon, YoutubeIcon } from 'lucide-react'
 import Image from 'next/image'
@@ -21,52 +22,85 @@ export default function Footer() {
                 />
               </div>
             </div>
-            <p className='body-3 text-[#B5B5B5]'>Site officiel de l'Archidiocèse de Moncton</p>
+            <p className='body-3 text-[#B5B5B5]'>
+              <Text className='text-inherit' keyString='footer_site_description' />
+            </p>
           </div>
           <div className='space-y-4'>
-            <h2 className='font-medium text-xs md:text-sm xl:text-base text-white uppercase'>Actualités</h2>
+            <h2 className='font-medium text-xs md:text-sm xl:text-base text-white uppercase'>
+              <Text className='text-inherit' keyString='news' />
+            </h2>
             <ul className='space-y-2 lg:space-y-4 body-3'>
-              <li className='text-[#B5B5B5]'>Nouvelles diocésaines</li>
               <li className='text-[#B5B5B5]'>
-                <Link href="/evenements">Evènements à venir</Link>
+                <Text className='text-inherit' keyString='nouvelles_diocesaines' />
+              </li>
+              <li className='text-[#B5B5B5]'>
+                <Link href="/evenements">
+                  <Text className='text-inherit' keyString='evenements_venir' />
+                </Link>
               </li>
               <li className='text-[#B5B5B5]'>Archives</li>
             </ul>
           </div>
 
           <div className='space-y-4'>
-            <h2 className='font-medium text-xs md:text-sm xl:text-base text-white uppercase'>Paroisse</h2>
+            <h2 className='font-medium text-xs md:text-sm xl:text-base text-white uppercase'>
+              <Text className='text-inherit' keyString='paroisse' />
+            </h2>
             <ul className='space-y-2 lg:space-y-4 body-3'>
               <li className='text-[#B5B5B5]'>
-                <Link href="/paroisses">Touver une Paroisse</Link>
+                <Link href="/paroisses">
+                  <Text className='text-inherit' keyString='trouver_paroisse' />
+                </Link>
               </li>
               <li className='text-[#B5B5B5]'>
-                <Link href="/horaires-messes">Horaires des messes</Link>
+                <Link href="/horaires-messes">
+                  <Text className='text-inherit' keyString='horaires_messe' />
+                </Link>
               </li>
               <li className='text-[#B5B5B5]'>
-                <Link href="/clerges">Equipe paroissiale</Link>
+                <Link href="/clerges">
+                  <Text className='text-inherit' keyString='equipe_paroissiale' />
+                </Link>
               </li>
             </ul>
           </div>
 
           <div className='space-y-4'>
-            <h2 className='font-medium text-xs md:text-sm xl:text-base text-white uppercase'>Ressources</h2>
+            <h2 className='font-medium text-xs md:text-sm xl:text-base text-white uppercase'>
+                <Text className='text-inherit' keyString='ressources' />
+            </h2>
             <ul className='space-y-2 lg:space-y-4 body-3 '>
-              <li className='text-[#B5B5B5]'>Prières</li>
-              <li className='text-[#B5B5B5]'>Textes réligieux</li>
-              <li className='text-[#B5B5B5]'>Formations</li>
-              <li className='text-[#B5B5B5]'>Documents officiels</li>
+              <li className='text-[#B5B5B5]'>
+                <Text className='text-inherit' keyString='prieres' />
+              </li>
+              <li className='text-[#B5B5B5]'>
+                <Text className='text-inherit' keyString='textes_religieux' />
+              </li>
+              <li className='text-[#B5B5B5]'>
+                <Text className='text-inherit' keyString='formations' />
+              </li>
+              <li className='text-[#B5B5B5]'>
+                <Text className='text-inherit' keyString='documents_officiels' />
+              </li>
             </ul>
           </div>
 
           <div className='space-y-4'>
-            <h2 className='font-medium text-xs md:text-sm xl:text-base text-white uppercase'>A propos</h2>
+            <h2 className='font-medium text-xs md:text-sm xl:text-base text-white uppercase'>
+              <Text className='text-inherit' keyString='a_propos' />
+            </h2>
             <ul className='space-y-2 lg:space-y-4 body-3'>
-              <li className='text-[#B5B5B5]'>Notre histoire</li>
-              <li className='text-[#B5B5B5]'>Missions et valeurs</li>
-
               <li className='text-[#B5B5B5]'>
-                <Link href="/clerges">Le Clergé</Link>
+                <Text className='text-inherit' keyString='footer_notre_histoire' />
+              </li>
+              <li className='text-[#B5B5B5]'>
+                <Text className='text-inherit' keyString='missions_valeurs' />
+              </li>
+              <li className='text-[#B5B5B5]'>
+                <Link href="/clerges">
+                  <Text className='text-inherit' keyString='clerge' />
+                </Link>
               </li>
 
             </ul>
@@ -79,7 +113,7 @@ export default function Footer() {
                 <Link className='text-[#B5B5B5]' href="https://www.google.com/maps/place/224+Rue+St.+George,+Moncton,+NB+E1C+5J4,+Canada/@46.090801,-64.781807,17z/data=!3m1!4b1!4m6!3m5!1s0x4ca0b93b01f859a1:0xd74f8270dc13186e!8m2!3d46.090801!4d-64.781807!16s%2Fg%2F11c3q4b2z7?entry=ttu&g_ep=EgoyMDI0MTAyOS4wIKXMDSoASAFQAw%3D%3D">224 St-George, Moncton, NB. E1C 0V1</Link>
               </li>
               <li>
-                <Link className='text-[#B5B5B5]' href="tel:+(506)857-9531">Tél: (506) 857-9531</Link>
+                <Link className='text-[#B5B5B5]' href="tel:+(506)857-9531"><Text className='text-inherit' keyString='phone' />: (506) 857-9531</Link>
               </li>
               <li>
                 <Link className='break-words text-[#B5B5B5]' href="mailto:webmestre@diocesemoncton.ca">webmestre@diocesemoncton.ca</Link>
@@ -91,8 +125,10 @@ export default function Footer() {
       <div className='bg-[#190F10]'>
         <div className='container max-margin py-6  flex flex-col md:flex-row md:justify-between gap-5 md:gap-3'>
           <div className='order-1 md:order-2 pt-1 '>
-            <p className='body-3 text-center md:text-left text-[#B5B5B5]'>© {date.getFullYear()} Diocèse Moncton - Tous droits reservés</p>
-            <p className='body-3 text-center md:text-left text-[#B5B5B5] text-xs'>Développé par <a href="http://www.linkedin.com/in/hadriengayap/" target="_blank" rel="noopener noreferrer">Acadie Intelligent Inc.</a> E-mail: <a href="mailto:hadrien.gayap@acadieintelligent.ca">hadrien.gayap@acadieintelligent.ca</a> </p>
+            <p className='body-3 text-center md:text-left text-[#B5B5B5]'>© {date.getFullYear()} <Text className='text-inherit' keyString='droit_reserver' /></p>
+            <p className='body-3 text-center md:text-left text-[#B5B5B5] text-xs'>
+              <Text className='text-inherit' keyString='developper_par' />
+              <a href="http://www.linkedin.com/in/hadriengayap/" target="_blank" rel="noopener noreferrer">Acadie Intelligent Inc.</a> E-mail: <a href="mailto:hadrien.gayap@acadieintelligent.ca">hadrien.gayap@acadieintelligent.ca</a> </p>
           </div>
           <div className='md:order-2  flex justify-center items-center gap-4 lg:gap-6'>
             <FacebookIcon className="h-4 w-4 md:h-6 md:w-6 !text-[#B5B5B5]" />
