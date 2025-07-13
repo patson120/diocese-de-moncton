9c666f33fcbbf5caf261bd92a3d91050b0c14976
patson120 <patrickkennenl@gmail.com>
2025-07-13 20:28:56 +0100

    Mise à jour de la traduction

--- src/app/[locale]/(overview)/mariage-sacrements/page.tsx
+++ src/app/[locale]/(overview)/mariage-sacrements/page.tsx
@@ -1,17 +1,18 @@
 'use client'
 
 import { HeroSectionSecond } from '@/components/sections/hero-second'
-import { ArrowUpRight } from 'lucide-react'
+import { useTranslations } from 'next-intl'
 import Image from 'next/image'
 
 export default function Page() {
+    const t = useTranslations("mariage_sacrements")
     return (
         <main>
             {/* Hero section */}
             <HeroSectionSecond
                 image='/assets/img/sacrements.png'
-                title="Le sacrement du mariage"
-                subtitle="Une alliance d'amour bénie par Dieu"
+                title={t("hero_titre")}
+                subtitle={t("hero_desc")}
             />
             <div className='mt-10 md:mt-20'></div>
             <section className="container max-margin py-0">
@@ -28,10 +29,10 @@ export default function Page() {
                     </div>
                     <div className='order-1 lg:order-2 col-span-full lg:col-span-3 flex flex-col justify-center items-center'>
                         <div>
-                            <h1 className='heading-4 font-extrabold mb-4'>Qu'est-ce que le sacrement du mariage ?</h1>
+                            <h1 className='heading-4 font-extrabold mb-4'>{t("section_1_titre")}</h1>
                             <div className='space-y-2'>
-                                <p className='body-2 text-gray'>Le mariage sacramental est un don réciproque que se font l’homme et la femme. Il est célébré en présence d’un prêtre ou d’un diacre, qui représente l’Église et appelle la bénédiction de Dieu sur le couple.</p>
-                                <p className='body-2 text-gray mt-3'>Ce sacrement est une alliance d’amour, une image vivante de l’amour du Christ pour son Église.</p>
+                                <p className='body-2 text-gray'>{t("section_1_p_1")}</p>
+                                <p className='body-2 text-gray mt-3'>{t("section_1_p_2")}</p>
                             </div>
                         </div>
                     </div>
@@ -41,52 +42,52 @@ export default function Page() {
             <div className='mt-10 md:mt-20'></div>
             <section className="container max-margin py-0 space-y-6" >
                 <div className='w-full lg:w-1/2'>
-                    <h2 className='heading-3 text-gray-900 mb-4'>Les conditions pour recevoir le sacrement du mariage</h2>
+                    <h2 className='heading-3 text-gray-900 mb-4'>{t("section_2_titre")}</h2>
                 </div>
                 <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
                     <div className='space-y-3'>
                         <div className='space-y-2'>
-                            <h1 className='body-1 text-[#1D0104] font-bold'>Le consentement libre</h1>
-                            <p className='body-2 text-gray'>Les futurs époux doivent dire "oui" en toute liberté, sans crainte ni contrainte.</p>
+                            <h1 className='body-1 text-[#1D0104] font-bold'>{t("section_2_sous_titre_1")}</h1>
+                            <p className='body-2 text-gray'>{t("section_2_p_1")}</p>
                         </div>
                     </div>
                     <div className='space-y-3'>
                         <div className='space-y-2'>
-                            <h1 className='body-1 text-[#1D0104] font-bold'>L’engagement exclusif et pour la vie</h1>
-                            <p className='body-2 text-gray'>Le mariage est une union indissoluble, ouverte à l’accueil des enfants.</p>
+                            <h1 className='body-1 text-[#1D0104] font-bold'>{t("section_2_sous_titre_2")}</h1>
+                            <p className='body-2 text-gray'>{t("section_2_p_2")}</p>
                         </div>
                     </div>
                     <div className='space-y-3'>
                         <div className='space-y-2'>
-                            <h1 className='body-1 text-[#1D0104] font-bold'>L’absence d’obstacles</h1>
-                            <p className='body-2 text-gray'>Aucun empêchement naturel ou ecclésial (comme un mariage préexistant ou des vœux de célibat) ne doit entraver l’union.</p>
+                            <h1 className='body-1 text-[#1D0104] font-bold'>{t("section_2_sous_titre_3")}</h1>
+                            <p className='body-2 text-gray'>{t("section_2_p_3")}</p>
                         </div>
                     </div>
                 </div>
             </section>
 
             <div className='mt-10 md:mt-20'></div>
-            <section className="container max-margin py-0 space-y-6" >
+            <section className="container max-margin py-0 space-y-6">
                 <div className='w-full lg:w-1/2'>
-                    <h2 className='heading-3 text-gray-900 mb-4'>Les fondements du mariage chrétien</h2>
+                    <h2 className='heading-3 text-gray-900 mb-4'>{t("section_3_titre")}</h2>
                 </div>
                 <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
                     <div className='space-y-3'>
                         <div className='space-y-2'>
-                            <h1 className='body-1 text-[#1D0104] font-bold'>Un "oui" libre et conscient</h1>
-                            <p className='body-2 text-gray'>Les époux s’engagent volontairement et en toute connaissance de cause.</p>
+                            <h1 className='body-1 text-[#1D0104] font-bold'>{t("section_3_sous_titre_1")}</h1>
+                            <p className='body-2 text-gray'>{t("section_3_p_1")}</p>
                         </div>
                     </div>
                     <div className='space-y-3'>
                         <div className='space-y-2'>
-                            <h1 className='body-1 text-[#1D0104] font-bold'>Une union exclusive et pour la vie</h1>
-                            <p className='body-2 text-gray'>Le mariage est une promesse d’amour fidèle et durable.</p>
+                            <h1 className='body-1 text-[#1D0104] font-bold'>{t('section_3_sous_titre_2')}</h1>
+                            <p className='body-2 text-gray'>{t('section_3_p_2')}</p>
                         </div>
                     </div>
                     <div className='space-y-3'>
                         <div className='space-y-2'>
-                            <h1 className='body-1 text-[#1D0104] font-bold'>L’ouverture à la vie</h1>
-                            <p className='body-2 text-gray'>Les époux accueillent les enfants comme un don de Dieu. Ces éléments font du mariage une image vivante de l’amour du Christ pour son Église.</p>
+                            <h1 className='body-1 text-[#1D0104] font-bold'>{t('section_3_sous_titre_3')}</h1>
+                            <p className='body-2 text-gray'>{t("section_3_p_3")}</p>
                         </div>
                     </div>
                 </div>
@@ -97,13 +98,13 @@ export default function Page() {
                 <div className='grid grid-cols-1 lg:grid-cols-5 gap-7 md:gap-6 lg:gap-12 md:py-4 lg:py-8'>
                     <div className='col-span-full lg:col-span-3 flex flex-col justify-center items-center'>
                         <div>
-                            <h1 className='heading-4 font-extrabold mb-4'>Se préparer au sacrement du mariage</h1>
+                            <h1 className='heading-4 font-extrabold mb-4'>{t("section_4_titre")}</h1>
                             <div className='space-y-4'>
-                                <p className='body-2 text-gray'>Les couples qui désirent se marier à l'Église sont invités à suivre des cours de préparation au mariage. Ces sessions permettent de :</p>
+                                <p className='body-2 text-gray'>{t('section_4_p_1')}</p>
                                 <ul className='ml-3'>
-                                    <li className='body-2 text-gray list-disc'>Comprendre le sens profond du mariage chrétien.</li>
-                                    <li className='body-2 text-gray list-disc'>Réfléchir aux défis de la vie conjugale.</li>
-                                    <li className='body-2 text-gray list-disc'>Renforcer la communication et la spiritualité du couple. Contactez votre paroisse pour connaître les dates et les modalités des sessions de préparation.</li>
+                                    <li className='body-2 text-gray list-disc'>{t("section_4_li_1")}</li>
+                                    <li className='body-2 text-gray list-disc'>{t("section_4_li_2")}</li>
+                                    <li className='body-2 text-gray list-disc'>{t("section_4_li_3")}</li>
                                 </ul>
                             </div>
                         </div>
@@ -124,8 +125,8 @@ export default function Page() {
             <div className='mt-10 md:mt-20'></div>
             <section className='container max-margin py-0'>
                 <div className='w-full lg:w-1/2'>
-                    <h2 className='heading-3 text-gray-900 mb-4'>Vous avez des questions ?</h2>
-                    <p className='body-2 text-gray'>Si vous avez des questions sur le sacrement de réconciliation ou si vous souhaitez rencontrer un prêtre pour une confession, n'hésitez pas à nous contacter.</p>
+                    <h2 className='heading-3 text-gray-900 mb-4'>{t("section_5_titre")}</h2>
+                    <p className='body-2 text-gray'>{t("section_5_p_1")}</p>
                 </div>
             </section>
             <div className='mt-5 md:mt-10'></div>
