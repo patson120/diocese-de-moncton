dc67d29eaf1af250b92d7f46f77af1ee5dad8e6a
patson120 <patrickkennenl@gmail.com>
2025-09-02 15:45:42 +0100

    Ajout de Google analytics sur le site web

--- src/app/[locale]/(overview)/layout.tsx
+++ src/app/[locale]/(overview)/layout.tsx
@@ -38,6 +38,23 @@ export default async function RootLayout({
 
   return (
     <html lang={locale}>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${inter.className} antialiased select-none`}>
         <NextIntlClientProvider messages={messages}>
