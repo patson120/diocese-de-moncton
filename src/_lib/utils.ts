e6ba6c499500c1d963002823f1712da409501c9a
patson120 <patrickkennenl@gmail.com>
2025-09-05 17:27:10 +0100

    Mise en place de la traduction au niveau des des dates sur l'ensemble du site

--- src/_lib/utils.ts
+++ src/_lib/utils.ts
@@ -1,21 +1,28 @@
 import { clsx, type ClassValue } from 'clsx';
+import { useLocale } from 'next-intl';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
 
-export const formatDateToLocal = (
+export const formatDateToLocal = async (
     dateStr: string,
-    locale: string = 'fr-FR',
+    //locale: string = 'fr-FR',
     pattern: "numeric" | "2-digit" | "long" | "short" | "narrow" = "short",
-) => {
+    ) => {
+    const localeLang = useLocale()
+    const language = localeLang === 'fr' ? "fr-FR": "en-EN"
+
+    // console.log(localeLang);
+    
+    
     const date = new Date(dateStr);
     const options: Intl.DateTimeFormatOptions = {
         day: 'numeric',
         month: pattern,
         year: 'numeric',
     };
-    const formatter = new Intl.DateTimeFormat(locale, options);
+    const formatter = new Intl.DateTimeFormat(language, options);
     return formatter.format(date);
 };
