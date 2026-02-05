017fbbfe2d878c9e57d1f858d9cf4e33f8a4614c
patson120 <patrickkennenl@gmail.com>
2026-02-05 18:15:19 -0400

    Mise à jour du footer

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const withNextIntl = createNextIntlPlugin(
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // output: 'export',
+    // distDir: 'build',
     eslint: {
         ignoreDuringBuilds: true,
     },
