1eb358e8714291234179091f34866e5cb32b9ddc
patson120 <patrickkennenl@gmail.com>
2026-03-03 03:43:44 -0400

    Mise à jour du layout 2

--- src/app/[locale]/(overview)/layout.tsx
+++ src/app/[locale]/(overview)/layout.tsx
@@ -23,11 +23,12 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({
   children,
-  params: { locale }
+  params
 }: Readonly<{
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }>) {
+  const { locale } = await params;
   // Ensure that the incoming `locale` is valid
   if (!routing.locales.includes(locale as Language)) {
     notFound();
