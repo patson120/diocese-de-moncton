d13a04e4634538a17dcf16a29588eff3599d9795
patson120 <patrickkennenl@gmail.com>
2026-03-19 06:49:19 -0300

    Correction de text et suppression de la section Newsletter dans toute les pages

--- src/app/[locale]/(overview)/layout.tsx
+++ src/app/[locale]/(overview)/layout.tsx
@@ -66,7 +66,7 @@ export default async function RootLayout({
             {children}
 
             {/* Newsletter */}
-            <Newsletter />
+            {/* <Newsletter /> */}
 
             {/* Footer */}
             <Footer />
