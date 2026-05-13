3d123bf7cb2c850f5895f35110c5abc1c87dc4cc
patson120 <patrickkennenl@gmail.com>
2026-05-13 11:18:47 -0300

    Mise à jour du menu mouvement

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -178,12 +178,104 @@ export const mouvements: MenuType[] = [
         image: '/assets/icons/people.png',
         titre_fr: 'Chevaliers de Colomb',
         titre_en: 'Knights of Columbus',
-        description_fr: 'Lorem ipsum dolor sit amet consectetur.',
-        description_en: 'Lorem ipsum dolor sit amet consectetur.',
-        link: 'https://www.diocesemoncton.ca/fr/chevaliers-de-colomb'
+        description_fr: "Une vie de foi  en action, une vie d'audace dans la fraternité, une vie qui vaut d'être véçu.",
+        description_en: "A life of faith in action, a life of courage in fellowship, a life worth living.",
+        link: 'https://www2.kofc.org/un/fr/index.html'
     },
-   /*  {
+   
+    {
         id: 2,
+        image: '/assets/icons/people.png',
+        titre_fr: 'Mouvements des Femmes Chrétiennes',
+        titre_en: 'Catholic Women\'s League (C.W.L)',
+        description_fr: 'Mouvement des femmes de tout âge peu importe leur situation et leur culture.',
+        description_en: 'A movement of women of all ages, regardless of their circumstances or cultural background.',
+        link: 'https://www.cwl.ca'
+    },
+    {
+        id: 3,
+        image: '/assets/icons/people.png',
+        titre_fr: 'Laudato Si',
+        titre_en: 'Laudato Si',
+        description_fr: '',
+        description_en: '',
+        link: 'https://laudatosimovement.org/fr'
+    },
+    {
+        id: 4,
+        image: '/assets/icons/people.png',
+        titre_fr: 'Cursillo',
+        titre_en: 'Cursillo',
+        description_fr: "Faire vivre dans l'espace de deux ou trois jours l'enseignement fondamental du Christ.",
+        description_en: "To bring Christ's fundamental teachings to life over the course of two or three days.",
+        link: 'https://www.cursillos.ca/indexfr.php'
+    },
+    {
+        id: 5,
+        image: '/assets/icons/people.png',
+        titre_fr: 'Développement et paix',
+        titre_en: 'Development and peace',
+        description_fr: "Organisme officiel de solidarité internationale de l'Eglise catholique.",
+        description_en: "The official international solidarity organization of the Catholic Church.",
+        link: 'https://devp.org/fr/'
+    },
+    {
+        id: 6,
+        image: '/assets/icons/people.png',
+        titre_fr: 'Legion de Marie',
+        titre_en: 'Marie\'s Legion',
+        description_fr: "L'étendard de la Légion de Marie est dressé aux quatre coins du globe grâce à son fondateur Frank Duff",
+        description_en: "The banner of the Legion of Mary flies in every corner of the globe thanks to its founder, Frank Duff",
+        link: 'https://legionofmary.ie/'
+    },
+    {
+        id: 7,
+        image: '/assets/icons/people.png',
+        titre_fr: "Propagation de la foi et Services d'animation missionnaire",
+        titre_en: 'Spreading the Faith and Missionary Outreach Services',
+        description_fr: "Comité qui coordonne les différents intervenants de l'action missionnaire.",
+        description_en: "A committee that coordinates the various stakeholders involved in missionary work.",
+        link: 'https://opmcanada.ca/oppf'
+    },
+    {
+        id: 8,
+        image: '/assets/icons/people.png',
+        titre_fr: 'Renouveau charismatique',
+        titre_en: 'Charismatic renewal',
+        description_fr: "Le mouvement a pour vocation d'incarner une culture de Pentecôte.",
+        description_en: 'The movement aims to embody a Pentecostal culture.',
+        link: 'https://renouveaucharismatique.ca/'
+    },
+    {
+        id: 9,
+        image: '/assets/icons/people.png',
+        titre_fr: 'Service de préparation au mariage',
+        titre_en: 'Pre-marriage counseling service',
+        description_fr: "Préparer les couples fiancés au mariage chrétien.",
+        description_en: "Prepare engaged couples for Christian marriage.",
+        link: 'https://diocese-de-moncton.vercel.app/fr/contact'
+    },
+    {
+        id: 10,
+        image: '/assets/icons/people.png',
+        titre_fr: 'Société St. Vincent de Paul',
+        titre_en: 'St. Vincent de Paul Society',
+        description_fr: "Notre travail est fondé sur l'amour du prochain et la justice.",
+        description_en: 'Our work is based on love for others and justice.',
+        link: 'https://ssvp.ca/fr/'
+    },
+    {
+        id: 11,
+        image: '/assets/icons/people.png',
+        titre_fr: 'Vivre et aimer - Rencontre conjugale',
+        titre_en: 'Live and love - Mariage Encounter',
+        description_fr: "Les couples mariés sont appelés à vivre l'amour inconditionnel...",
+        description_en: 'Married couples are called to live out unconditional love...',
+        link: 'https://www.vivreetaimer.com/'
+    },
+
+    /*  {
+        id: 1,
         image: '/assets/icons/people.png',
         titre_fr: 'Comité diocésain de la bible (Société biblique)',
         titre_en: 'Diocesan Bible Committee (Bible Society)',
@@ -191,80 +283,26 @@ export const mouvements: MenuType[] = [
         description_en: 'Lorem ipsum dolor sit amet consectetur.',
         link: 'https://www.diocesemoncton.ca/fr/comite-diocesain-de-la-bible-societe-biblique'
     }, */
-    {
-        id: 3,
-        image: '/assets/icons/people.png',
-        titre_fr: 'Mouvements des Femmes Chrétiennes',
-        titre_en: 'Catholic Women\'s League (C.W.L)',
-        description_fr: 'Lorem ipsum dolor sit amet consectetur.',
-        description_en: 'Lorem ipsum dolor sit amet consectetur.',
-        link: 'https://www.diocesemoncton.ca/fr/mouvement-des-femmes-chretiennes'
-    },
-    {
-        id: 4,
-        image: '/assets/icons/people.png',
-        titre_fr: 'Cursillo',
-        titre_en: 'Cursillo',
-        description_fr: 'Lorem ipsum dolor sit amet consectetur.',
-        description_en: 'Lorem ipsum dolor sit amet consectetur.',
-        link: 'https://www.diocesemoncton.ca/fr/cursillo'
-    },
-    {
-        id: 5,
-        image: '/assets/icons/people.png',
-        titre_fr: 'Développement et paix',
-        titre_en: 'Development and peace',
-        description_fr: 'Lorem ipsum dolor sit amet consectetur.',
-        description_en: 'Lorem ipsum dolor sit amet consectetur.',
-        link: 'https://www.diocesemoncton.ca/fr/developpement-et-paix'
-    },
     /* {
-        id: 6,
-        image: '/assets/icons/people.png',
-        titre_fr: 'La vie montante',
-        titre_en: 'Life on the rise',
-        description_fr: 'Lorem ipsum dolor sit amet consectetur.',
-        description_en: 'Lorem ipsum dolor sit amet consectetur.',
-        link: 'https://www.diocesemoncton.ca/fr/la-vie-montante'
-    }, */
-    {
-        id: 7,
-        image: '/assets/icons/people.png',
-        titre_fr: 'Legion de Marie',
-        titre_en: 'Marie\'s Legion',
-        description_fr: 'Lorem ipsum dolor sit amet consectetur.',
-        description_en: 'Lorem ipsum dolor sit amet consectetur.',
-        link: 'https://www.diocesemoncton.ca/fr/legion-de-marie'
-    },
-    {
-        id: 8,
-        image: '/assets/icons/people.png',
-        titre_fr: 'Renouveau charismatique',
-        titre_en: 'Charismatic renewal',
-        description_fr: 'Lorem ipsum dolor sit amet consectetur.',
-        description_en: 'Lorem ipsum dolor sit amet consectetur.',
-        link: 'https://www.diocesemoncton.ca/fr/renouveau-charistmatique'
-    },
-    {
-        id: 9,
+        id: 2,
         image: '/assets/icons/people.png',
         titre_fr: 'Service d\'animation missionnaire',
         titre_en: 'Missionary animation service',
         description_fr: 'Lorem ipsum dolor sit amet consectetur.',
         description_en: 'Lorem ipsum dolor sit amet consectetur.',
         link: 'https://www.diocesemoncton.ca/fr/service-danimation-missionnaire'
-    },
-    {
-        id: 10,
+    }, */
+    /* {
+        id: 3,
         image: '/assets/icons/people.png',
         titre_fr: 'Protocole de minsitère responsable',
         titre_en: 'Responsible Ministry Protocol',
         description_fr: 'Lorem ipsum dolor sit amet consectetur.',
         description_en: 'Lorem ipsum dolor sit amet consectetur.',
         link: 'https://www.diocesemoncton.ca/fr/responsible-ministry-protocol'
-    },
+    }, */
     /* {
-        id: 11,
+        id: 4,
         image: '/assets/icons/people.png',
         titre_fr: 'Scouts',
         titre_en: 'Scouts',
@@ -272,33 +310,15 @@ export const mouvements: MenuType[] = [
         description_en: 'Lorem ipsum dolor sit amet consectetur.',
         link: 'https://www.diocesemoncton.ca/fr/scouts'
     }, */
-    {
-        id: 12,
+    /* {
+        id: 5,
         image: '/assets/icons/people.png',
-        titre_fr: 'Service de préparation au mariage',
-        titre_en: 'Pre-marriage counseling service',
+        titre_fr: 'La vie montante',
+        titre_en: 'Life on the rise',
         description_fr: 'Lorem ipsum dolor sit amet consectetur.',
         description_en: 'Lorem ipsum dolor sit amet consectetur.',
-        link: 'https://www.diocesemoncton.ca/fr/service-de-preparation-au-mariage'
-    },
-    {
-        id: 13,
-        image: '/assets/icons/people.png',
-        titre_fr: 'Société St. Vincent de Paul',
-        titre_en: 'St. Vincent de Paul Society',
-        description_fr: 'Lorem ipsum dolor sit amet consectetur.',
-        description_en: 'Lorem ipsum dolor sit amet consectetur.',
-        link: 'https://www.diocesemoncton.ca/fr/societe-st-vincent-de-paul'
-    },
-    {
-        id: 14,
-        image: '/assets/icons/people.png',
-        titre_fr: 'Vivre et aimer - Rencontre conjugale',
-        titre_en: 'Live and love - Rencontre conjugale',
-        description_fr: 'Lorem ipsum dolor sit amet consectetur.',
-        description_en: 'Lorem ipsum dolor sit amet consectetur.',
-        link: 'https://www.diocesemoncton.ca/fr/vivre-et-aimer'
-    },
+        link: 'https://www.diocesemoncton.ca/fr/la-vie-montante'
+    }, */
 ]
 
 export const ressources: MenuType[] = [
